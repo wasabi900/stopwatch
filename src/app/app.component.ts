47804fd77d3d311bc2b0de783fb267bde88dd7f1
wasabi900 <b.miki517@gmail.com>
2024-03-18 00:07:45 +0100

    init app, implement basic stopwatch functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,54 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faPlay, faStop, faPause, faFlag } from '@fortawesome/free-solid-svg-icons';
+import { interval, map, tap } from 'rxjs';
+import { TimerService } from './core/timer.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FontAwesomeModule],
+  providers: [TimerService],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
+
+  timerService = inject(TimerService);
+
   title = 'stopwatch';
+
+  faPlay = faPlay;
+  faStop = faStop;
+  faPause = faPause;
+  faFlag = faFlag;
+
+
+  time = this.timerService.getCurrentTime();
+  running$ = this.timerService.isRunning();
+  timer$ = this.timerService.timer();
+
+  laps$ = this.timerService.getLaps();
+
+  start(isRunning: boolean) {
+    this.timerService.setRunning(isRunning);
+
+    if (!isRunning) {
+      this.time = this.timerService.getCurrentTime();
+    }
+
+  }
+
+  reset() {
+    this.timerService.resetTimer();
+    this.time = this.timerService.getCurrentTime();
+  }
+
+  lap() {
+    this.timerService.addLap();
+  }
+
 }
---
+++ src/app/core/timer.service.ts
@@ -0,0 +1,59 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, interval, map } from 'rxjs';
+
+@Injectable()
+export class TimerService {
+
+  private isRunningSubject = new BehaviorSubject<boolean>(false);
+  private laps = new BehaviorSubject<{diff: number, time: number}[]>([]);
+
+  private readonly intervalValue = 10;
+  readonly timeZoneOffSet = new Date().getTimezoneOffset() * 60000;
+  private time: number = this.timeZoneOffSet;
+
+  timer() {
+    return interval(this.intervalValue).pipe(
+      map(() => {
+        this.time += this.intervalValue;
+        return this.time;
+      })
+    );
+  }
+
+  resetTimer() {
+    this.time = this.timeZoneOffSet;
+    this.setRunning(false);
+    this.laps.next([]);
+  }
+
+  getCurrentTime(): number {
+    return this.time;
+  }
+
+  isRunning() {
+    return this.isRunningSubject.asObservable();
+  }
+
+  setRunning(isRunning: boolean) {
+    this.isRunningSubject.next(isRunning);
+  }
+
+  addLap() {
+    const lap = {
+      diff: this.getDiff(),
+      time: this.getCurrentTime()
+    }
+
+    this.laps.next([...this.laps.value, lap]);
+  }
+
+  getLaps() {
+    return this.laps.asObservable();
+  }
+
+  getDiff() {
+    const length = this.laps.value.length;
+    return length > 0 ?  this.getCurrentTime() - this.laps.value[length -1].time : this.getCurrentTime();
+  }
+
+}
